cafb54debdcf8ffaa71cc038d3f9b49153017381
MineBill <minebill@protonmail.com>
2021-08-28 10:19:10 +0300

    Refactor fxmanifest.lua and don't assume database name in .sql file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,44 +1,31 @@
-fx_version 'adamant '
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
 
-ui_page 'html/form.html'
+author "MineBill"
+version "1.0.0"
+
+ui_page "html/form.html"
 
 files {
-	'html/form.html',
-	'html/img/seal.png',
-	'html/img/document.jpg',
-	'html/img/signature.png',
-	'html/img/cursor.png',
-	'html/css.css',
-	'html/language_en.js',
-	'html/language_gr.js',
-	'html/language_br.js',
-	'html/language_de.js',
-	'html/language_fr.js',
-	'html/script.js',
-	'html/jquery-3.4.1.min.js',
+	"html/**.*",
+}
+
+shared_scripts {
+	"@es_extended/locale.lua",
+	"locales/**.lua",
+	"config.lua",
 }
 
 server_scripts {
-	'config.lua',
-	'@mysql-async/lib/MySQL.lua',
-	'@es_extended/locale.lua',
-	'locales/gr.lua',
-	'locales/en.lua',
-	'locales/br.lua',
-	'locales/de.lua',
-	'locales/fr.lua',
-	'server.lua',
+	"@mysql-async/lib/MySQL.lua",
+	"server.lua",
 }
 
 client_scripts {
-	'config.lua',
-	'@es_extended/locale.lua',
-	'locales/gr.lua',
-	'locales/en.lua',
-	'locales/br.lua',
-	'locales/de.lua',
-	'locales/fr.lua',
-	'GUI.lua',
-	'client.lua',
+	"GUI.lua",
+	"client.lua",
+}
+
+dependencies {
+	"es_extended"
 }
